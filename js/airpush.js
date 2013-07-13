d4b450ea73ec3d6b3213c194be592548034eeb39
Chris A. Taylor <chris@gameclosure.com>
2013-07-12 19:49:09 -0700

    Remove track

--- js/airpush.js
+++ js/airpush.js
@@ -1,10 +1,4 @@
 var Airpush = Class(function () {
-	this.track = function (name, data) {
-		logger.log("{airpush} track: ", name, data);
-		NATIVE && NATIVE.plugins && NATIVE.plugins.sendEvent &&
-			NATIVE.plugins.sendEvent("AirpushPlugin", "airpush",
-				JSON.stringify({ eventName: name, params: data }));
-	};
 });
 
 exports = new Airpush();
